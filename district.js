96e5404ffb4fbcf4a97347bb745c6ffcc34cc655
siddhartha-441 <111590548+siddhartha-441@users.noreply.github.com>
2023-07-18 11:58:50 +0530

    Update district.js

--- district.js
+++ district.js
@@ -1,22 +1,21 @@
-// import path from 'path';
-// import python from 'python-shell';
 function get_district(){
-   
-    var python = require("python-shell")
-    var path = require("path")
+    define(function (require) {
+        var python = require("python-shell")
+        var path = require("path")
+        
+        var district_name = document.getElementById("district_name").value
+        document.getElementById("district_name").value = "";
 
-    var district_name = document.getElementById("district_name").value
-    document.getElementById("district_name").value = "";
+        var options = {
+            scriptPath : path.join(_dirname,'/../backend/'),
+            args : [district_name]
+        }
 
-    var options = {
-        scriptPath : path.join(_dirname,'/../backend/'),
-        args : [district_name]
-    }
+        var dis = new python('district.py',options);
 
-    var dis = new python('district.py',options);
-
-    dis.on('message', function(message) {
-        swal(message);
-    })
+        dis.on('message', function(message) {
+            swal(message);
+        })
+    });  
 }
 
